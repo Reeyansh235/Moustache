01387aaab7d5f8cce0153875075b3f8ed4dced3d
Reeyansh235 <71835087+Reeyansh235@users.noreply.github.com>
2021-04-20 16:43:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/tCF7mqZQ/download-5-removebg-preview.png')
+}
+
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-11;
+        noseY = results[0].pose.nose.y+3;
+        console.log("nose x = " + results[0].pose.nose.x)
+        console.log("nose y = " + results[0].pose.nose.y)
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
+    //fill(255,0,0);
+    //stroke(0,255,0);
+    //circle(noseX, noseY, 20);
+}
+function take_snapshot(){
+    save('MyFilterImage');
+}
+
